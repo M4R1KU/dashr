388275b4ca25cb1d6be688197e800c687c15fea7
Mario Kunz <mario.kunz@bit.admin.ch>
2018-06-15 12:56:47 +0200

    testing core-js 3

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,10 +19,10 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es6/symbol';
+/*import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
- import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-int';
 import 'core-js/es6/parse-float';
 import 'core-js/es6/number';
 import 'core-js/es6/math';
@@ -32,14 +32,16 @@ import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
+import 'core-js/es6/set';*/
+
+import 'core-js';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+// import 'core-js/es6/reflect';
+// import 'core-js/es7/reflect';
 
 
 /**
